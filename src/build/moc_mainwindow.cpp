e61299bf317ef4336b9dc181499140a90dc73db1
Maksim <americaacop@gmail.com>
2024-06-05 21:09:16 +0700

    moving to github

---
+++ src/build/moc_mainwindow.cpp
@@ -0,0 +1,238 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../3D_Viewer/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[32];
+    char stringdata0[282];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 8), // "fileopen"
+QT_MOC_LITERAL(2, 20, 0), // ""
+QT_MOC_LITERAL(3, 21, 10), // "load_model"
+QT_MOC_LITERAL(4, 32, 6), // "remove"
+QT_MOC_LITERAL(5, 39, 8), // "geometry"
+QT_MOC_LITERAL(6, 48, 5), // "color"
+QT_MOC_LITERAL(7, 54, 8), // "vertices"
+QT_MOC_LITERAL(8, 63, 5), // "edges"
+QT_MOC_LITERAL(9, 69, 10), // "projection"
+QT_MOC_LITERAL(10, 80, 4), // "save"
+QT_MOC_LITERAL(11, 85, 4), // "move"
+QT_MOC_LITERAL(12, 90, 9), // "but_color"
+QT_MOC_LITERAL(13, 100, 8), // "make_scr"
+QT_MOC_LITERAL(14, 109, 8), // "make_gif"
+QT_MOC_LITERAL(15, 118, 8), // "save_gif"
+QT_MOC_LITERAL(16, 127, 8), // "rotate_x"
+QT_MOC_LITERAL(17, 136, 8), // "rotate_y"
+QT_MOC_LITERAL(18, 145, 8), // "rotate_z"
+QT_MOC_LITERAL(19, 154, 6), // "move_x"
+QT_MOC_LITERAL(20, 161, 6), // "move_y"
+QT_MOC_LITERAL(21, 168, 6), // "move_z"
+QT_MOC_LITERAL(22, 175, 9), // "scale_obj"
+QT_MOC_LITERAL(23, 185, 9), // "type_line"
+QT_MOC_LITERAL(24, 195, 12), // "type_project"
+QT_MOC_LITERAL(25, 208, 13), // "type_vertices"
+QT_MOC_LITERAL(26, 222, 12), // "last_setting"
+QT_MOC_LITERAL(27, 235, 8), // "size_ver"
+QT_MOC_LITERAL(28, 244, 9), // "size_edge"
+QT_MOC_LITERAL(29, 254, 11), // "auto_rotate"
+QT_MOC_LITERAL(30, 266, 11), // "switch_menu"
+QT_MOC_LITERAL(31, 278, 3) // "off"
+
+    },
+    "MainWindow\0fileopen\0\0load_model\0remove\0"
+    "geometry\0color\0vertices\0edges\0projection\0"
+    "save\0move\0but_color\0make_scr\0make_gif\0"
+    "save_gif\0rotate_x\0rotate_y\0rotate_z\0"
+    "move_x\0move_y\0move_z\0scale_obj\0type_line\0"
+    "type_project\0type_vertices\0last_setting\0"
+    "size_ver\0size_edge\0auto_rotate\0"
+    "switch_menu\0off"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      29,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  159,    2, 0x08 /* Private */,
+       3,    0,  160,    2, 0x08 /* Private */,
+       4,    0,  161,    2, 0x08 /* Private */,
+       5,    0,  162,    2, 0x08 /* Private */,
+       6,    0,  163,    2, 0x08 /* Private */,
+       7,    0,  164,    2, 0x08 /* Private */,
+       8,    0,  165,    2, 0x08 /* Private */,
+       9,    0,  166,    2, 0x08 /* Private */,
+      10,    0,  167,    2, 0x08 /* Private */,
+      11,    0,  168,    2, 0x08 /* Private */,
+      12,    0,  169,    2, 0x08 /* Private */,
+      13,    0,  170,    2, 0x08 /* Private */,
+      14,    0,  171,    2, 0x08 /* Private */,
+      15,    0,  172,    2, 0x08 /* Private */,
+      16,    0,  173,    2, 0x08 /* Private */,
+      17,    0,  174,    2, 0x08 /* Private */,
+      18,    0,  175,    2, 0x08 /* Private */,
+      19,    0,  176,    2, 0x08 /* Private */,
+      20,    0,  177,    2, 0x08 /* Private */,
+      21,    0,  178,    2, 0x08 /* Private */,
+      22,    0,  179,    2, 0x08 /* Private */,
+      23,    0,  180,    2, 0x08 /* Private */,
+      24,    0,  181,    2, 0x08 /* Private */,
+      25,    0,  182,    2, 0x08 /* Private */,
+      26,    0,  183,    2, 0x08 /* Private */,
+      27,    0,  184,    2, 0x08 /* Private */,
+      28,    0,  185,    2, 0x08 /* Private */,
+      29,    0,  186,    2, 0x08 /* Private */,
+      30,    1,  187,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   31,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->fileopen(); break;
+        case 1: _t->load_model(); break;
+        case 2: _t->remove(); break;
+        case 3: _t->geometry(); break;
+        case 4: _t->color(); break;
+        case 5: _t->vertices(); break;
+        case 6: _t->edges(); break;
+        case 7: _t->projection(); break;
+        case 8: _t->save(); break;
+        case 9: _t->move(); break;
+        case 10: _t->but_color(); break;
+        case 11: _t->make_scr(); break;
+        case 12: _t->make_gif(); break;
+        case 13: _t->save_gif(); break;
+        case 14: _t->rotate_x(); break;
+        case 15: _t->rotate_y(); break;
+        case 16: _t->rotate_z(); break;
+        case 17: _t->move_x(); break;
+        case 18: _t->move_y(); break;
+        case 19: _t->move_z(); break;
+        case 20: _t->scale_obj(); break;
+        case 21: _t->type_line(); break;
+        case 22: _t->type_project(); break;
+        case 23: _t->type_vertices(); break;
+        case 24: _t->last_setting(); break;
+        case 25: _t->size_ver(); break;
+        case 26: _t->size_edge(); break;
+        case 27: _t->auto_rotate(); break;
+        case 28: _t->switch_menu((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 29)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 29;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 29)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 29;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
